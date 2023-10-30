8ee26775535b053d6e8e31be27b60e83682dc985
trakan tantee <87230169+AJtrakan@users.noreply.github.com>
2023-10-30 11:19:13 +0700

    Add files via upload

---
+++ extension.js
@@ -0,0 +1,13 @@
+({
+    name: "T-ZX-THERMO", // Category Name
+    description: "Non-Contact Temperature Sensor",
+    author: "AJ.TAY@INEX",
+    category: "Sensors",
+    version: "1.0.0",
+    icon: "/static/icon.png", // Category icon
+    color: "#27AE60", // Category color (recommend some blocks color)
+    blocks: [ // Blocks in Category
+        "max90614_object_temp",
+        "max90614_ambient_temp",
+    ]
+});
